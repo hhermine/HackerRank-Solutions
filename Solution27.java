a4b1138965592b5441acec259a49f3ff088f211c
Hermine (Emily) Hovhannisyan <emilyhovhannisyan@yahoo.com>
2016-08-24 15:32:07 -0400

    Add files via upload

---
+++ Solution27.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    System.out.println("5");
+    System.out.println("15 10");
+    System.out.println("-2 0 -4 4 0 5 10 -3 4 3 6 -5 11 0 -8");
+    System.out.println("8 5");
+    System.out.println("-4 -4 3 -1 0 -3 -2 -8");
+    System.out.println("20 15");
+    System.out.println("-2 0 -4 4 0 5 10 11 -20 10 0 0 -6 -3 4 3 6 -5 11 -8");
+    System.out.println("6 3");
+    System.out.println("3 -1 0 -3 -2 -8");
+    System.out.println("16 15");
+    System.out.println("-2 0 -4 4 0 5 10 11 -20 10 0 0 -6 -3 4 3");
+  }
+}
