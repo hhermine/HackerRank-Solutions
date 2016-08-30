901d07cc2421e78689197c5fb3404239af4dbf7e
Hermine (Emily) Hovhannisyan <emilyhovhannisyan@yahoo.com>
2016-08-29 22:38:10 -0400

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,23 @@
+/* Declare second integer, double, and String variables. */
+int j;
+double e;
+String m;
+String f;
+
+/* Read and save an integer, double, and String to your variables.*/
+j=scan.nextInt();
+e=scan.nextDouble();
+m=scan.nextLine();
+f= scan.nextLine();
+
+/* Print the sum of both integer variables on a new line. */
+System.out.println(i+j);
+
+
+/* Print the sum of the double variables on a new line. */
+System.out.println(d+e);
+
+/* Concatenate and print the String variables on a new line;
+	the 's' variable above should be printed first. */
+
+System.out.println(s+f);
